f7f0c63883da8e20de13f98cd9f0773c4e5900db
meriem <hlaili.meriem@gmail.com>
2013-02-14 02:35:43 -0800

    Com1 14/02 11:35

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RHFactoryAuthentif::Application.config.secret_token = '3d541cbb1a73c5697314dc8ab48ae63b01b3e94496196f9a96fcf67bc64744cb5f15249b8c5f0d7229e1857faca92c8f67fd407834fd6d1a2d650459aee6a5a6'
